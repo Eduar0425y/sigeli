6c199ae7bc4df4c1de97f91254839b8c87dcc4d3
Eduar0425y <xavieravendano9@gmail.com>
2022-11-03 16:42:58 -0400

    Conversion del proyecto a Maven e inclusión del Dao de la Carrera

---
+++ Sigeli/src/main/java/main/java/com/mycompany/sigeliapp/dao/Constantes.java
@@ -0,0 +1,68 @@
+package main.java.com.mycompany.sigeliapp.dao;
+
+
+public class Constantes {
+    public static final String URL="jdbc:mysql://localhost:3306/";
+    public static final String DATABASE="sigeli";
+    public static final String USER="root";
+    public static final String PASSWORD="";
+    
+    public static final String T_ROL = "";
+    public static final String TR_ID = "";
+    public static final String TR_ROL = "";
+    
+    public static final String T_CARRERA = "carrera";
+    public static final String TC_ID = "id_carrera";
+    public static final String TC_CARREERA = "nombre_carrera";
+    
+    
+    public static final String T_CATEGORIA = "";
+    public static final String TCAT_ID = "";
+    public static final String TCAT_CATEGORIA = "";
+    
+    public static final String T_CATLIBRO = "";
+    public static final String TCL_ISBN = "";
+    public static final String TCL_IDCATEGORIA = "";
+    
+    public static final String T_ESTADO = "";
+    public static final String TE_ID = "";
+    public static final String TE_ESTADO = "";
+    
+    public static final String T_LIBRO = "";
+    public static final String TL_ISBN = "";
+    public static final String TL_NOMBRE = "";
+    public static final String TL_AUTOR = "";
+    public static final String TL_EDICION = "";
+    public static final String TL_ANO = "";
+    public static final String TL_ESTANTE = "";
+    public static final String TL_FILA = "";
+    public static final String TL_IDESTADO = "";
+    
+    public static final String T_MULTA = "";
+    public static final String TM_ID = "";
+    public static final String TM_DOCUMENTO = "";
+    public static final String TM_IDPRESTAMO = "";
+    public static final String TM_VALOR = "";
+    public static final String IDESTADO = "";
+    
+    public static final String T_PERSONA = "";
+    public static final String TP_DOCUMENTO = "";
+    public static final String TP_NOMBRE = "";
+    public static final String TP_IDCARRERA = "";
+    public static final String TP_EMAIL = "";
+    public static final String TP_TELEFONO = "";
+    
+    public static final String T_PRESTAMO = "";
+    public static final String TPR_ID = "";
+    public static final String TPR_DOCUMENTO = "";
+    public static final String TPR_ISBN = "";
+    public static final String TPR_FECHAPRESTAMO = "";
+    public static final String TPR_FECHAENTREGA = "";
+    public static final String TPR_IDESTADO = "";
+    
+    public static final String T_USUARIO = "";
+    public static final String TU_DOCUMENTO = "";
+    public static final String TU_CLAVE = "";
+    public static final String TU_CARGO = "";
+    
+}
