33ad8a45ae171c51955a865a5f5ce51e6e7d57c2
Eduar0425y <xavieravendano9@gmail.com>
2022-11-03 12:13:56 -0400

    ...

--- siegliApp/src/sigeliaplicacion/dao/Constantes.java
+++
@@ -1,9 +0,0 @@
-package sigeliaplicacion.dao;
-
-
-public class Constantes {
-    public static final String URL="jdbc:mysql://localhost:3306/";
-    public static final String DATABASE="";
-    public static final String USER="root";
-    public static final String PASSWORD="";
-}
